6ebdaa4ac7a450286286c78badc9626cb06fef26
Cuong Nguyen <>
2022-06-23 20:38:45 +0700

    refactor:  Restructure the project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  initialWindowMetrics,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+import {ProductDetailScreen} from '@screens';
+
+const App = () => {
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <ProductDetailScreen />
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
---
+++ src/screens/product-detail-screen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {View} from 'react-native';
+
+const ProductDetailScreen = () => {
+  return <View></View>
+}
+
+export {ProductDetailScreen}
